7e62336b2ccd8cde8ccb4d45ff687ac5991ccdcc
Junior Delgado <juprogramador@gmail.com>
2025-05-01 02:28:33 -0100

    Styling the Registration/Login Screen

--- src/pages/RegisterLoginScreen/RegisterLoginScreen.jsx
+++ src/pages/RegisterLoginScreen/RegisterLoginScreen.jsx
@@ -1,26 +1,45 @@
 import React from 'react';
+import './style.css'
 
 function RegisterLoginScreen() {
   return (
     <div>
         <header>
             <img src="" alt="Logotipo" />
-            <button></button>
-            <button></button>
-            <button></button>
+            <button>HOME</button>
+            <button>HOME</button>
+            <button>HOME</button>
             <input type="text" placeholder="Pesquisar" />
             <img src="" alt="Favorites" />
             <img src="" alt="Carrinho" />
             <img src="" alt="Perfil" />
         </header>
-        <div>
+        <div className='container'>
             <form action="" method="post">
                 <h1>Cadastre-se Aqui</h1>
                 <input type="text" name="nome" placeholder="Nome" />
                 <input type="email" name="email" placeholder="Email" />
                 <input type="password" name="senha" placeholder="Senha" />
+                <p><strong>Sexo: </strong></p>
+                <div className='gender-div'>
+                    <input type="radio" id="male" name="sexo" value="homem" />
+                    <label for="male"><strong>Masculino</strong></label>
+                    <input type="radio" id="female" name="sexo" value="mulher" />
+                    <label for="female"><strong>Femenino</strong></label>
+                    <input type="radio" id="outro" name="sexo" value="outro" />
+                    <label for="outro"><strong>Outro</strong></label>
+                </div>
+                
+                <p><strong>Data de Nascimento</strong></p>
+                <input type="date" name="nascimento" />
+                <p><strong>Endereço</strong></p>
+                <input type="text" name="endereco" placeholder="País" />
+                <input type="text" name="endereco" placeholder="Cidade" />
+                <input type="text" name="endereco" placeholder="Zona" />
+
                 <input type="submit" value="Cadastrar" />
-            </form>    
+            </form>
+            <p>OU</p> 
             <form action="" method="post">
                 <h1>Faça Login</h1>
                 <button>Login com Google</button> 
